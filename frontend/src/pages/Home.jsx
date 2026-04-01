9e628790c1770f42c46234a79ea1b141d3116ef9
MatheusBrandon <reki.brand22@gmail.com>
2026-03-31 23:18:23 -0400

    Pasta Frontend
    http://localhost:5173/
    http://localhost:5173/criar-conta
    http://localhost:5173/cadastro
    http://localhost:5173/login

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>Use Flashcards</h1>
+      <p>Acesse questões do professor</p>
+
+      <button>Criar Conta</button>
+      <button>Entrar</button>
+    </div>
+  );
+}
+
+export default Home;
